d36e3d4a7d2268e12a8544bf40b72a2d17ed5be2
ameletto <meamymli@gmail.com>
2021-04-18 18:02:21 -0400

    Plotting sine of target angles.

--- analyze.py
+++ analyze.py
@@ -1,10 +1,14 @@
 import numpy
 import matplotlib.pyplot
 
-backLegSensorValues=numpy.load('data/backLegSensorValues.npy') 
-frontLegSensorValues=numpy.load('data/frontLegSensorValues.npy')
-matplotlib.pyplot.plot(backLegSensorValues, label='Back Leg', linewidth=2)
-matplotlib.pyplot.plot(frontLegSensorValues, label='Front Leg')
+# backLegSensorValues=numpy.load('data/backLegSensorValues.npy') 
+# frontLegSensorValues=numpy.load('data/frontLegSensorValues.npy')
+# matplotlib.pyplot.plot(backLegSensorValues, label='Back Leg', linewidth=2)
+# matplotlib.pyplot.plot(frontLegSensorValues, label='Front Leg')
 
-matplotlib.pyplot.legend()
+targetAngles=numpy.load('data/targetAngles.npy')
+matplotlib.pyplot.plot(targetAngles, numpy.sin(targetAngles))
+matplotlib.pyplot.xlabel('Angle [rad]')
+matplotlib.pyplot.ylabel('sin(x)')
+
 matplotlib.pyplot.show()
