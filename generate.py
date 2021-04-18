5e292cf657c4d65c137b38d687d2f6296c179a6c
Amy Li <me.amymli@gmail.com>
2021-04-18 11:01:52 -0400

    Generating a link at the origin.

--- generate.py
+++ generate.py
@@ -1,10 +1,17 @@
 import pyrosim.pyrosim as pyrosim
 
-x=1
-y=1
+x=0.5
+y=0.5
 z=0.5
 
-pyrosim.Start_SDF("boxes.sdf")
+length=1
+width=1
+height=1
+
+pyrosim.Start_SDF("world.sdf")
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+
+"""
 for i in range (6):
     y=y+1
     x=1
@@ -20,5 +27,6 @@ for i in range (6):
             width=width*0.9
             height=height*0.9
             z=z+height
+"""
 pyrosim.End()
 
