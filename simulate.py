0c8ce7ac0b88067a4cba6a187ddde85545fab1ec
ameletto <meamymli@gmail.com>
2021-04-18 12:15:06 -0400

    Adding a touch sensor.

--- simulate.py
+++ simulate.py
@@ -1,6 +1,7 @@
 import pybullet as p
 import time
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -8,7 +9,9 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 planeId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate("body.urdf")
 for i in range (1000):
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     time.sleep(1/60)
 p.disconnect()
