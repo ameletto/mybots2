f34554d3375518a3dc222c76b5244ee14e6a76af
ameletto <meamymli@gmail.com>
2021-04-18 14:11:49 -0400

    Adding a legend.

--- analyze.py
+++ analyze.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot
 
 backLegSensorValues=numpy.load('data/backLegSensorValues.npy') 
 frontLegSensorValues=numpy.load('data/frontLegSensorValues.npy')
-matplotlib.pyplot.plot(backLegSensorValues)
-matplotlib.pyplot.plot(frontLegSensorValues)
+matplotlib.pyplot.plot(backLegSensorValues, label='Back Leg', linewidth=2)
+matplotlib.pyplot.plot(frontLegSensorValues, label='Front Leg')
 
+matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
