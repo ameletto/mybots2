b54d41c8ddd512e10ace3e3a3dbb6d7f3f4bb399
ameletto <meamymli@gmail.com>
2021-04-18 12:34:35 -0400

    Creating a numpy vector.

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import pybullet as p
 import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
+import numpy
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -10,7 +11,10 @@ planeId = p.loadURDF("plane.urdf")
 planeId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate("body.urdf")
-for i in range (1000):
+backLegSensorValues = numpy.zeros(10000)
+print(backLegSensorValues)
+exit()
+for i in range (10000):
     p.stepSimulation()
     backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     print(backLegTouch)
