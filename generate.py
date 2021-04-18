ca0e62aca17ac90f617ecbfd0398402a301386e4
ameletto <meamymli@gmail.com>
2021-04-18 11:15:26 -0400

    Creating a link at the origin.

--- generate.py
+++ generate.py
@@ -11,22 +11,22 @@ height=1
 pyrosim.Start_SDF("world.sdf")
 pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
 
-"""
-for i in range (6):
-    y=y+1
-    x=1
-    for j in range (6):
-        length=1
-        width=1
-        height=1
-        z=0.5
-        x=x+1
-        for k in range (10):
-            pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-            length=length*0.9
-            width=width*0.9
-            height=height*0.9
-            z=z+height
-"""
+
+# for i in range (6):
+#     y=y+1
+#     x=1
+#     for j in range (6):
+#         length=1
+#         width=1
+#         height=1
+#         z=0.5
+#         x=x+1
+#         for k in range (10):
+#             pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+#             length=length*0.9
+#             width=width*0.9
+#             height=height*0.9
+#             z=z+height
+
 pyrosim.End()
 
