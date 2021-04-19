093030b9d21242d0317e0892f86de476fce28e78
ameletto <meamymli@gmail.com>
2021-04-18 23:41:57 -0400

    Making robot move like a sine wave.

--- simulate.py
+++ simulate.py
@@ -16,10 +16,10 @@ pyrosim.Prepare_To_Simulate("body.urdf")
 backLegSensorValues = numpy.zeros(1000)
 frontLegSensorValues = numpy.zeros(1000)
 
-targetAngles=numpy.linspace(-numpy.pi, numpy.pi, 1000)
-numpy.save('data/targetAngles.npy', targetAngles)
+targetAngles=numpy.linspace(-numpy.pi , numpy.pi, 1000)
+motorValues=numpy.pi/4*numpy.sin(targetAngles)
+# numpy.save('data/targetAngles.npy', targetAngles)
 
-exit()
 for i in range (1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
@@ -29,17 +29,17 @@ for i in range (1000):
     bodyIndex = robot,
     jointName = "Torso_BackLeg",
     controlMode = p.POSITION_CONTROL,
-    targetPosition = random.uniform(-math.pi/2.0, math.pi/2.0),
+    targetPosition = motorValues[i],
     maxForce = 40)
     
     pyrosim.Set_Motor_For_Joint(
     bodyIndex = robot,
     jointName = "Torso_FrontLeg",
     controlMode = p.POSITION_CONTROL,
-    targetPosition = random.uniform(-math.pi/2.0, math.pi/2.0),
+    targetPosition = motorValues[i],
     maxForce = 40)
 
-    time.sleep(1/100)
+    time.sleep(1/500)
 p.disconnect()
 numpy.save('data/backLegSensorValues.npy', backLegSensorValues)
 numpy.save('data/frontLegSensorValues.npy', frontLegSensorValues)
